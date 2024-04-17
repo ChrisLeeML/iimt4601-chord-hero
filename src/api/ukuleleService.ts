ea297134e08ebce421231c2aec256cf20bd30154
jihoo529 <jihoolee529@gmail.com>
2024-04-18 01:50:37 +0800

    Added CreateCreator function

--- src/api/ukuleleService.ts
+++ src/api/ukuleleService.ts
@@ -1,6 +1,8 @@
 "use server";
 import { cookieBasedClient } from "@/app/layout";
 import { listUkuleles } from "@/src/graphql/queries";
+import { createCreator } from "@/src/graphql/mutations";
+import { create } from "@mui/material/styles/createTransitions";
 
 /**
  * Fetches ukulele data using the GraphQL query listUkuleles.
@@ -18,6 +20,23 @@ export const GetUkulele = async () => {
     } catch (error) {
     console.error('Error fetching ukuleles:', error);
     return null;
+    }
+}
+
+export const CreateCreator = async (name: string) => {
+    try {
+    const response = await cookieBasedClient.graphql({
+        query: createCreator,
+        variables: { 
+            input: {
+                name: name,
+                schoolID: '0'
+            }
+        }
+    });
+    console.log("Created creator", response);
+    } catch (error) {
+    console.error('Error fetching ukuleles:', error);
     }
 }
 
