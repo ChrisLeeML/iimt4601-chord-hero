e32b966c7eb80df0a24c8442b35a654b09bf0338
jihoo529 <jihoolee529@gmail.com>
2024-04-24 21:39:47 +0800

    added test.tsx for jest testing

---
+++ src/__tests__/queries.test.tsx
@@ -0,0 +1,8 @@
+describe('GraphQL Query Tests', () => {
+  it('fetches owner by ID', () => {
+    expect(true).toBeTruthy();
+  });
+  it('list owners', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -3,6 +3,7 @@
 // this is an auto generated file. This will be overwritten
 
 import * as APITypes from "../API";
+import { gql } from "@apollo/client";
 type GeneratedQuery<InputType, OutputType> = string & {
   __generatedQueryInput: InputType;
   __generatedQueryOutput: OutputType;
